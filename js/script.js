8a5d2f267b33ead90757260a33ef9090de44e677
Kevin Garcia <ermianrazor@gmail.com>
2024-09-27 13:48:15 -0500

    BUG FIXES

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
-let eamilReg = document.getElementById("emailReg");
-let formReg = document.getElementById("formReg");
-let emailInvalid = document.getElementById("emailValidatorText")
 let nav = document.querySelector("nav");
-
-console.log(emailInvalid)
-
-formReg.addEventListener("submit", e=> {
-    e.preventDefault()
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    if (!regexEmail.test(emailReg.value)) {
-        emailInvalid.classList.remove("d-none")
-        emailInvalid.classList.add("d-inline")
-    }
-})
-
 
 window.addEventListener("scroll", function(){
     if(window.scrollY > 100) {
