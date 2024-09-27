f11e0ca1d14d448dcb37a411a5befc6289891f74
Kevin Garcia <ermianrazor@gmail.com>
2024-09-27 13:59:18 -0500

    Delete test

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let nav = document.querySelector("nav");
-
-window.addEventListener("scroll", function(){
-    if(window.scrollY > 100) {
-        nav.classList.add("bg-nav-dark" , "shadow");
-    } else {
-        nav.classList.remove("bg-nav-dark",  "shadow")
-    }
-})
